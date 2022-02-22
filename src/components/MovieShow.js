3b80fa9152e4144cb44f938f052f0c4aeae8625d
Wen Xiao <wennstyle@gmail.com>
2022-02-22 14:49:43 -0500

    commit tree 2f46c464f4614e2e904a94a67819de2d8b91c48c

---
+++ src/components/MovieShow.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useParams } from "react-router-dom"
+
+function MovieShow({movies}) {
+
+    const params = useParams();
+    console.log(useParams)
+
+  return (
+    <div>
+      <h3>{movies[params.movieId].title}</h3>
+    </div>
+  );
+}
+
+export default MovieShow;
--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+  //const match = useRouteMatch();
+  //console.log(match)
+
   return (
     <div>
       <MoviesList movies={movies} />
+
+{/* We have to use match.url instead of / to equal path because we do not want the message in this path
+to show in the home page */}
+      <Route exact path={useRouteMatch().url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+
+      <Route path={`${useRouteMatch().url}/:movieId`}>
+        <MovieShow movies={movies}/>
+      </Route>
     </div>
   );
 }
